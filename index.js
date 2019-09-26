9e8f8da5dc2d8a1d2ab764f4d579f6492a4b2889
Sinh Nguyen <snguyen@opswat.com>
2019-09-26 17:55:59 +0700

    video 12

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var shortid = require('shortid');
-
-var adapter = new FileSync('db.json');
-var db = low(adapter)
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
-
 var app = express();
+var userRoutes = require('./routes/user.route');
 var port = 3000;
 
 app.set('views', './views');
@@ -19,6 +9,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -26,41 +17,7 @@ app.get('/', function(req, res){
     });
 });
 
-app.get('/users', function(req, res){
-    res.render('./users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/:id', function(req, res){
-    var id = parseInt(req.params.id);
-    var user = db.get('users').find({id: id}).value();
-    res.render("users/view", {
-        user: user
-    });
-});
-
-app.get('/users/search', function(req, res){
-    var q = req.query.q;
-    var matchedUser = users.filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-
-    res.render('users/index', {
-        users: matchedUser
-    });
-});
-
-app.get('/users/create', function(req, res){
-    res.render('users/create')
-})
-
-app.post('/users/create', function(req, res){
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-})
-
+app.use('/users', userRoutes);
 app.listen(port, function(){
     console.log("Server listening on port " + port);
 });
